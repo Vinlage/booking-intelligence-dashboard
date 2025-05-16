3eaea83e6765643dd811c1910c42478ab62d7b35
Vinicius Lage <vinicius_clage@yahoo.com.br>
2025-05-16 14:33:10 -0300

    feat: change booking module to save through typeorm

--- backend/src/booking/booking.service.ts
+++ backend/src/booking/booking.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { Booking } from './entities/booking.entity';
-
-const mockBookings: Booking[] = [];
-let idcounter = 1;
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BookingService {
+  constructor(
+    @InjectRepository(Booking)
+    private bookingRepository: Repository<Booking>,
+  ) {}
+
   create(createBookingDto: CreateBookingDto) {
-    const booking: Booking = {
-      id: idcounter++,
+    const booking = {
       patient: createBookingDto.patient,
       therapist: createBookingDto.therapist,
       date: createBookingDto.date,
     };
-    mockBookings.push(booking);
+    return this.bookingRepository.save(booking);
   }
 
   findAll() {
-    return mockBookings;
+    return this.bookingRepository.find();
   }
 
   findOne(id: number) {
--- backend/src/booking/entities/booking.entity.ts
+++ backend/src/booking/entities/booking.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Booking {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   patient: string;
+
+  @Column()
   therapist: string;
+
+  @Column()
   date: string;
 }
