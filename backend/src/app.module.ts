6a846013ee09ff6c4ae16ae30c8811f737393dc2
Vinicius Lage <vinicius_clage@yahoo.com.br>
2025-05-27 20:40:48 -0300

    feat: add database url env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingModule } from './booking/booking.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'myuser',
-      password: 'mypassword',
-      database: 'booking_db',
+      url: process.env.DATABASE_URL,
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'myuser',
+      // password: 'mypassword',
+      // database: 'booking_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      autoLoadEntities: true,
     }),
     BookingModule,
   ],
